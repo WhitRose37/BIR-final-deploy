1a1b7413b471da85c762afbd1656c441a2228287
Thapanawat <137152332+WhitRose37@users.noreply.github.com>
2025-12-02 09:34:47 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverExternalPackages: ["@prisma/client", "bcrypt"],
+  serverExternalPackages: ["bcrypt"],
   images: {
     remotePatterns: [
       {
